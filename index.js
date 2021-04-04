b6b00a1c3e0bec23726f4d6b3af8e50c03e113a7
Hannah M <hannahemalm616@gmail.com>
2021-04-04 15:30:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const append = ["Milo", "Otis", "Garfield"]
 append.push("Odie")
 
 const prepend = ["Milo", "Otis", "Garfield"]
-
+prepend.unshift("Odie")
 const removeLast = ["Milo", "Otis", "Garfield"]
 
 const removeFirst = ["Milo", "Otis", "Garfield"]
